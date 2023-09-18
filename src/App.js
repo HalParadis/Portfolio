abf2ca2b3d7dde11f4aa4ac7ac113a3cdb792391
HalParadis <halparadis@gmail.com>
2023-09-18 13:54:05 -0700

    boilerplate code complete

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { useState, useEffect } from 'react';
+import { Route, Link, useHistory } from 'react-router-dom';
+
+const App = () => {
+  const history = useHistory();
+
+  return (
+    <div>
+        <header>
+
+        </header>
+    </div>
+  )
+}
+
+export default App;
