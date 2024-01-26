57ee70c82d3013f62031572f19efb0c7663484ca
HalParadis <halparadis@gmail.com>
2024-01-25 19:37:54 -0800

    Project component collapsable functional with transition. Needs finetuning.

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,15 +1,37 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { Route, Link, useHistory } from 'react-router-dom';
 
 const Project = ({description, name, url}) => {
+  const [isExpanded, setIsExpanded] = useState(false);
+  const [buttonArrow, setButtonArrow] = useState('>');
+  const contentRef = useRef(null);
+
+  useEffect(() => {
+    const classList = contentRef.current.classList;
+    if (isExpanded) {
+      setButtonArrow('˅');
+      classList.replace('collapsed', 'expanded');
+    }
+    else {
+      setButtonArrow('>');
+      classList.replace('expanded', 'collapsed');
+    }
+  }, [isExpanded])
+
   return (
     <div className='project'>
-      <button type='button' className='expand-button'>
-        <span>&gt;</span>
+      <button 
+        type='button' 
+        className='expand-button'
+        onClick={() => setIsExpanded(!isExpanded)}
+      >
+        <span>{buttonArrow}</span>
         <h3 className='project-name'>{name}</h3>
       </button>
-      <a className='project-anchor' href={url}>{url}</a>
-      <p>{description}</p>
+      <div className='project-content collapsed' ref={contentRef}>
+        <a className='project-anchor' href={url}>{url}</a>
+        <p>{description}</p>
+      </div>
     </div>
   )
 }
