df23a7d7bd5cd4f6ac4b8b87bf45825d2fa7cdd6
HalParadis <halparadis@gmail.com>
2024-02-06 15:33:10 -0800

    Contact page styling v.1 complete

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,13 +1,26 @@
 import React, { useState, useEffect } from 'react';
 import { Route, Link, useHistory } from 'react-router-dom';
+import resume from '../resume/Harold-Paradis-Resume-2.pdf';
 
 const Contact = () => {
   return (
     <div className='contact-info'>
       {/* <a className='contact-anchor' href="tel:5105015043">(510)501-5043</a> */}
-      <a className='contact-anchor' href="mailto: halparadis@gmail.com">halparadis@gmail.com</a>
-      <a className='contact-anchor' href="http://www.linkedin.com/in/hal-paradis">LinkedIn</a>
-      <a className='contact-anchor' href="https://github.com/HalParadis">GitHub</a>
+      <div className='contacts-container'>
+        <h3>Email:</h3>
+        <div className='contacts-subcontainer'>
+          <a className='contact-anchor link-button' href="mailto: halparadis@gmail.com">halparadis@gmail.com</a>
+        </div>
+      </div>
+      
+      <div className='contacts-container'>
+        <h3>Other:</h3>
+        <div className='contacts-subcontainer'>
+          <a className='contact-anchor link-button' href="http://www.linkedin.com/in/hal-paradis">LinkedIn</a>
+          <a className='contact-anchor link-button' href="https://github.com/HalParadis">GitHub</a>
+          <a className='contact-anchor link-button' href={resume} target='_blank'>Resume</a>
+        </div>
+      </div>
     </div>
   )
 }
